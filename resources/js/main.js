caa7d933c87311b53892477bb4103f2bb97b6ba4
Jasmine Ou <jasmineou99@gmail.com>
2018-05-29 00:28:49 -0400

    Flipping with back of polaroid design and close alignment

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,9 +1,32 @@
 $(window).ready(updateHeight);
 $(window).resize(updateHeight);
-
 function updateHeight() {
-    var newHeight = $('#button').outerHeight();
-    $('.flip-container').height(newHeight);
-    $('.front').height(newHeight);
-    $('.back').height(newHeight);
+    var height = [];
+    var x = 0;
+    $('.myimg').each(function () {
+        height[x] = $(this).height()+61.9;
+        console.log(height[x]);
+        x++;
+    });
+
+    x = 0;
+    $('.flip-container').each(function () {
+
+        $(this).height(height[x]);;
+        x++;
+    });
+
+    x = 0;
+    $('.front').each(function () {
+
+        $(this).height(height[x]);;
+        x++;
+    });
+
+    x = 0;
+    $('.back').each(function () {
+
+        $(this).height(height[x]);;
+        x++;
+    });
 }
