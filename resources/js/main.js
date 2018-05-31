ce2ef60df3be57e0a2e66df6846da37eeb840446
jou <jou@JYANG-LT>
2018-05-31 13:01:31 -0400

    Added responsive navbar with hamburger menu

--- resources/js/main.js
+++ resources/js/main.js
@@ -30,3 +30,12 @@ function updateHeight() {
         x++;
     });
 }
+
+function responsivenav() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
